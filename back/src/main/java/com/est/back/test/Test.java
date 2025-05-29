3080fad2729689c111874e5f127d5d3524379622
j1sooo <kjisoo1031@gmail.com>
2025-05-29 16:38:06 +0900

    test

---
+++ back/src/main/java/com/est/back/test/Test.java
@@ -0,0 +1,9 @@
+package com.est.back.test;
+
+import software.amazon.awssdk.services.ssm.endpoints.internal.Value;
+
+public class Test {
+    public static void main(String[] args) {
+        int i = 2;
+    }
+}
