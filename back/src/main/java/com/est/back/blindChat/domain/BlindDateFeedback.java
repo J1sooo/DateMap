db93e9b066355732ad4d5979eb221c927575aa50
wngkr <juhak0998@gmail.com>
2025-06-04 10:44:46 +0900

    feat: AI 소개팅 채팅 피드백 기능 구현 - Gemini AI api를 이용해 대화 내용 전체 피드백 - api 응답 잘라내기 , 피드백 DB 저장

---
+++ back/src/main/java/com/est/back/blindChat/domain/BlindDateFeedback.java
@@ -0,0 +1,46 @@
+package com.est.back.blindChat.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name= "blind_date_feedback")
+public class BlindDateFeedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 외래키 char_id
+    @Column(name = "char_id", nullable = false)
+    private Long charId;
+
+    @Column(name = "usn", nullable = false)
+    private Long usn;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "summary", length = 1000)
+    private String summary;
+
+    @Column(name = "feedback", length = 2000)
+    private String feedback;
+
+    @Column(name = "score")
+    private Integer score;
+
+
+}
---
+++ back/src/main/java/com/est/back/blindChat/repository/BlindDateFeedbackRepository.java
@@ -0,0 +1,8 @@
+package com.est.back.blindChat.repository;
+
+import com.est.back.blindChat.domain.BlindDateFeedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlindDateFeedbackRepository extends JpaRepository<BlindDateFeedback, Long> {
+
+}
