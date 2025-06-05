740620e5c930385d3353cdf9b1fb65dd642015e1
tetsuya <cinemamyg@gmail.com>
2025-06-05 01:09:48 -0500

    - 패키지 구조 변경  - DTO 로직 추가

--- back/src/main/java/com/est/back/repository/ChatroomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.est.back.repository;
-
-import com.est.back.domain.Chatroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
-}
