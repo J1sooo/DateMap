4b758eda43bc63eade0413957cd6fefadbe3433f
tetsuya <cinemamyg@gmail.com>
2025-06-05 01:09:48 -0500

    - 패키지 구조 변경  - DTO 로직 추가

--- back/src/main/java/com/est/back/repository/PartnerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.est.back.repository;
-
-import com.est.back.domain.Partner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartnerRepository extends JpaRepository<Partner, Long> {
-}
--- back/src/main/java/com/est/back/repository/RecommendRepository.java
+++
@@ -1,7 +0,0 @@
-package com.est.back.repository;
-
-import com.est.back.domain.Recommend;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecommendRepository extends JpaRepository<Recommend, Long> {
-}
