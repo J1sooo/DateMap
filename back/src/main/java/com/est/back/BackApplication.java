b67a706ce19745b40b5559e0a9a806ac4ca925ab
wngkr <juhak0998@gmail.com>
2025-06-10 14:41:10 +0900

    feat : 매주 월요일 자정 데이터 값 자동 삭제 -blind_date_character , chatroom , chatMessage삭제

--- back/src/main/java/com/est/back/BackApplication.java
+++ back/src/main/java/com/est/back/BackApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BackApplication {
