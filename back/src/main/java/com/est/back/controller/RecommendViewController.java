66ca45e3e29693e62a8bc48fcd96e0eccf357bbf
wngkr <juhak0998@gmail.com>
2025-06-10 11:41:55 +0900

    fix : URL 접근 차단 - 본인 채팅방이 아닌 경우 접근 제한 - 본인 채팅방이 아닌 경우 접근 시 리다이렉트 처리

--- back/src/main/java/com/est/back/controller/RecommendViewController.java
+++ back/src/main/java/com/est/back/controller/RecommendViewController.java
@@ -1,15 +1,15 @@
-package com.est.back.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class RecommendViewController {
-
-    @GetMapping("/recommend-setting")
-    public String viewPartnerSetting() {
-
-        return "recommendSetting";
-    }
-
-}
+//package com.est.back.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class RecommendViewController {
+//
+//    @GetMapping("/recommend-setting")
+//    public String viewPartnerSetting() {
+//
+//        return "recommendSetting";
+//    }
+//
+//}
