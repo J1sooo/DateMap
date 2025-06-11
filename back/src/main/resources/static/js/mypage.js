417aeef667b5d4549aa2ce8f9be68d099f1c6718
wngkr38 <124230135+wngkr38@users.noreply.github.com>
2025-06-11 14:29:43 +0900

    fix : mypage 종합분석 버튼 수정
    fix : mypage 종합분석 버튼 수정

--- back/src/main/resources/static/js/mypage.js
+++ back/src/main/resources/static/js/mypage.js
@@ -1,18 +1,22 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const button = document.getElementById("analyze-btn");
-//   const count = parseInt(button.dataset.count, 10); // 소개팅 횟수
-//   const today = new Date();
-//   const day = today.getDay()
-//   const dateKey = today.toISOString().slice(0, 10);
-//   const storageKey = `buttonClicked-${dateKey}`;
-//
-//   // 조건: 일요일 , 소개팅 5회 이상 , 아직 클릭 안 함
-//   if (day === 0 && count >= 5 && !localStorage.getItem(storageKey)) {
-//     button.style.display = "inline-block";
-//
-//     button.addEventListener("click", () => {
-//       localStorage.setItem(storageKey, "true");
-//       button.style.display = "none";
-//     });
-//   }
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("analyze-btn");
+  const count = parseInt(button.dataset.count, 10); // 소개팅 횟수
+  const today = new Date();
+  const day = today.getDay()
+  const dateKey = today.toISOString().slice(0, 10);
+  const storageKey = `buttonClicked-${dateKey}`;
+
+  // 조건: 일요일 , 소개팅 5회 이상 , 아직 클릭 안 함
+  // if (day === 0 && count >= 5 && !localStorage.getItem(storageKey)) {
+  // button.style.display = "inline-block";
+  //
+  //   button.addEventListener("click", () => {
+  //     localStorage.setItem(storageKey, "true");
+  //     button.style.display = "none";
+  //   });
+  // }
+  if (count >= 0) {
+    button.style.display = "inline-block";
+
+  }
+});
